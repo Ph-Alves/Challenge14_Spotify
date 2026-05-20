4f0d537844e7677fd406ee9e4693dff8167f1a72
Débora_Ferreira <deboracris29a@gmail.com>
2026-05-20 11:59:44 -0300

    feat: add tab bar

--- CloningSpotify/CloningSpotify/ProfileSearch/PlaylistSearchView.swift
+++ CloningSpotify/CloningSpotify/ProfileSearch/PlaylistSearchView.swift
@@ -8,134 +8,150 @@
 
 import SwiftUI
 
-struct PlaylistSearch: View {
+struct PlaylistSearchView: View {
     @State private var seartchText = ""
     @State private var likes = "1,629.529 likes"
     @State private var playlistTime = "6h 48m"
+    @Environment(\.horizontalSizeClass) var hSize
     
     var body: some View {
-        ScrollView {
+        ZStack{
+            Color.black.ignoresSafeArea()
             
-            VStack{
-                ViewThatFits(in: .horizontal){
+            ScrollView {
+                VStack{
+                    ViewThatFits(in: .horizontal){
+                        
+                        HStack {
+                            
+                            SearchBarView()
+                            
+                            Spacer(minLength: 7)
+                            
+                            Button {
+                                
+                                
+                            } label: {
+                                Text("Sort")
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.caption)
+                                    .fontWeight(.semibold)
+                                
+                            }
+                            .padding(EdgeInsets(top: 11, leading: 15, bottom: 11, trailing: 15))
+                            .background(RoundedRectangle(cornerRadius: 6).fill(Color("backgroundButton")))
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            
+                            SearchBarView()
+                            
+                            Spacer(minLength: 7)
+                            
+                            Button {
+                                
+                                
+                            } label: {
+                                Text("Sort")
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.caption)
+                                    .fontWeight(.semibold)
+                                
+                            }
+                            .padding(EdgeInsets(top: 11, leading: 15, bottom: 11, trailing: 15))
+                            .background(RoundedRectangle(cornerRadius: 6).fill(Color("backgroundButton")))
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 1)
+                    Image("playlistImage")
+                        .frame(width: 290, height: 288)
+                        .padding(.top, 30)
+                        .frame(alignment: .center)
                     
                     HStack {
-                        
-                        SearchBarView()
-                        
-                        Spacer(minLength: 7)
-                        
-                        Button {
-                            
-                            
-                        } label: {
-                            Text("Sort")
-                                .foregroundStyle(Color.white)
-                                .font(Font.caption)
-                                .fontWeight(.semibold)
-                            
-                        }
-                        .padding(EdgeInsets(top: 11, leading: 15, bottom: 11, trailing: 15))
-                        .background(RoundedRectangle(cornerRadius: 6).fill(Color("backgroundButton")))
+                        Text("Now and approved indie pop. Cover: No Rome")
+                            .foregroundStyle(Color.white)
+                            .font(Font.caption)
+                            .fontWeight(.light)
+                            .padding(.top)
+                            .padding(.leading)
+                        Spacer()
                     }
                     
-                    VStack(alignment: .leading) {
-                        
-                        SearchBarView()
-                        
-                        Spacer(minLength: 7)
-                        
-                        Button {
+                    HStack(alignment: .bottom) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image("spotifyIcon")
+                                Text("Spotify")
+                                    .foregroundStyle(Color.white)
+                                    .fontWeight(.bold)
+                                    .font(.default)
+                            }
                             
+                            HStack {
+                                Text(likes)
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.default)
+                                    .fontWeight(.light)
+                                Image(systemName: "circle.fill")
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.system(size: 5))
+                                Text(playlistTime)
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.default)
+                                    .fontWeight(.light)
+                            }
                             
-                        } label: {
-                            Text("Sort")
-                                .foregroundStyle(Color.white)
-                                .font(Font.caption)
-                                .fontWeight(.semibold)
+                            Spacer(minLength: 10)
                             
+                            HStack(spacing: 30){
+                                Image(systemName: "heart")
+                                    .foregroundStyle(Color.gray)
+                                    .font(Font.title2)
+                                Image(systemName: "arrow.down.circle")
+                                    .foregroundStyle(Color.gray)
+                                    .font(Font.title2)
+                                Image(systemName: "ellipsis")
+                                    .foregroundStyle(Color.gray)
+                                    .font(Font.title2)
+                            }
                         }
-                        .padding(EdgeInsets(top: 11, leading: 15, bottom: 11, trailing: 15))
-                        .background(RoundedRectangle(cornerRadius: 6).fill(Color("backgroundButton")))
+                        
+                        Spacer()
+                        Image("play")
+                        
+                        
                     }
-                }
-                .padding()
-                
-                Image("playlistImage")
-                    .frame(width: 290, height: 288)
-                    .padding(.top, 30)
-                    .frame(alignment: .center)
-                
-                HStack {
-                    Text("Now and approved indie pop. Cover: No Rome")
-                        .foregroundStyle(Color.white)
-                        .font(Font.caption)
-                        .fontWeight(.light)
-                        .padding(.top)
-                        .padding(.leading)
+                    .padding(.horizontal)
+                    
+                    SongsListView()
                     Spacer()
                 }
                 
-                HStack(alignment: .bottom) {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Image("spotifyIcon")
-                            Text("Spotify")
-                                .foregroundStyle(Color.white)
-                                .fontWeight(.bold)
-                                .font(.default)
-                        }
-                        
-                        HStack {
-                            Text(likes)
-                                .foregroundStyle(Color.white)
-                                .font(Font.default)
-                                .fontWeight(.light)
-                            Image(systemName: "circle.fill")
-                                .foregroundStyle(Color.white)
-                                .font(Font.system(size: 5))
-                            Text(playlistTime)
-                                .foregroundStyle(Color.white)
-                                .font(Font.default)
-                                .fontWeight(.light)
-                        }
-                        
-                        Spacer(minLength: 10)
-                        
-                        HStack(spacing: 30){
-                            Image(systemName: "heart")
-                                .foregroundStyle(Color.gray)
-                                .font(Font.title2)
-                            Image(systemName: "arrow.down.circle")
-                                .foregroundStyle(Color.gray)
-                                .font(Font.title2)
-                            Image(systemName: "ellipsis")
-                                .foregroundStyle(Color.gray)
-                                .font(Font.title2)
-                        }
-                    }
-                    
-                    Spacer()
-                    Image("play")
-                    
-                    
-                }
-                .padding(.horizontal)
                 
-                SongsListView()
-                Spacer()
             }
             
-            .background(Color.black)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                            .foregroundStyle(Color.white)
+                    }
+                    
+                }.sharedBackgroundVisibility(.hidden)
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .background(Color.black)
-        
+
     }
     
 }
 
 #Preview {
-    PlaylistSearch()
+    PlaylistSearchView()
 }
 
 
--- CloningSpotify/CloningSpotify/ViewController.swift
+++ CloningSpotify/CloningSpotify/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
-
+import SwiftUI 
 class ViewController: UIViewController {
-
+    let playlistSearch = UIHostingController(rootView: PlaylistSearchView())
+    
+    @IBOutlet var textFields: [UITextField]!
+    
+    
+    @IBAction func login(_ sender: Any) {
+        print("logado")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addChild(playlistSearch)
+        
+        view.addSubview(playlistSearch.view)
+        setUpConstraints()
+        
     }
-
-
+    
+    func setUpConstraints() {
+        playlistSearch.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            playlistSearch.view.topAnchor.constraint(equalTo: view.topAnchor),
+            playlistSearch.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            playlistSearch.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            playlistSearch.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
