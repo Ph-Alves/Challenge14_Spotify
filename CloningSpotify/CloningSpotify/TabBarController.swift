517f34aaf16208fc2cdcded7504ab16965bd5264
Rodrooj <rodrigobarbosapereira17@gmail.com>
2026-05-21 17:56:22 -0300

    chore: Biblioteca de músicas movida de pasta, Biblioteca integrada à TabBar

--- CloningSpotify/CloningSpotify/TabBarController.swift
+++ CloningSpotify/CloningSpotify/TabBarController.swift
@@ -24,13 +24,16 @@ class TabBarController: UITabBarController {
         profileSearchView.tabBarItem = UITabBarItem(title: "Playlist Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
         storyboardViewController.tabBarItem = UITabBarItem(title: "Storyboard", image: UIImage(systemName: "distribute.horizontal.fill"), tag: 1)
         albumScreen.tabBarItem = UITabBarItem(title: "Playlist", image: UIImage(systemName: "music.note"), tag: 2)
-        libraryView.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "books.vertical.fill"), tag: 0)
-
+        libraryView.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "books.vertical.fill"), tag: 3)
+        
+        let libraryNav = UINavigationController(rootViewController: libraryView)
+                libraryNav.isNavigationBarHidden = true
         
         viewControllers = [
             UINavigationController(rootViewController: profileSearchView),
             UINavigationController(rootViewController: storyboardViewController),
-            UINavigationController(rootViewController: albumScreen)
+            UINavigationController(rootViewController: albumScreen),
+            libraryNav
         ]
         tabBar.tintColor = selectedColor
         
